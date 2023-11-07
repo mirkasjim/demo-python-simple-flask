f9793aac138efbfacdf0c31223d919a92b8fe5a3
Bree Benesh <bree.benesh@amazee.io>
2023-11-07 16:25:33 -0500

    Modifying app to output ascii art from static/words.txt, which makes it easy to make a visible change during a simple demo.

--- aiohello.py
+++ aiohello.py
@@ -4,13 +4,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return """
+    words = "HELLO WORLD"
+
+    try:
+        with open('static/words.txt', 'r') as file:
+            words = file.read()
+    except FileNotFoundError:
+        words = "File 'words.txt' not found."
+
+    html_output = f"""
+
 <!DOCTYPE html>
 <html>
   <head>
     <link rel="stylesheet" type="text/css" rel="noopener" target="_blank" href="/static/style.css" />
   </head>
   <body>
+  <pre>{words}</pre>
   <img src="/static/aioheader.svg" width="800px" />
   </body>
 </html>"""
+    return html_output
